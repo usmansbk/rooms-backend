03dcf41c7e97011724392fd37279c13c817233a9
Usman Suleiman <usmansbk@gmail.com>
2021-11-28 23:34:41 +0100

    Setup rswag

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
