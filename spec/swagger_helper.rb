b4aaad88bfa78ab18931acac0c2bbe8d8bb6e8a1
Usman Suleiman <usmansbk@gmail.com>
2021-11-29 05:52:13 +0100

    Generate api docs

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -22,10 +22,10 @@ RSpec.configure do |config|
       paths: {},
       servers: [
         {
-          url: 'https://{defaultHost}',
+          url: 'http://{defaultHost}',
           variables: {
             defaultHost: {
-              default: 'localhost:3000'
+              default: '127.0.0.1:3000'
             }
           }
         }
