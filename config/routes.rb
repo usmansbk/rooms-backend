91615c8edc30d738bfa603bb31a160a0a8ccf3ed
Usman Suleiman <usmansbk@gmail.com>
2021-11-29 02:35:03 +0100

    Setup authentication with JWT

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, skip: :all
-  devise_scope :user do
-    post 'login', to: 'devise/sessions#create', defaults: { format: :json }
-    delete 'logout', to: 'devise/sessions#destroy', defaults: { format: :json }
-    post 'register', to: 'devise/registrations#create', defaults: { format: :json }
-  end
+  devise_for :users, defaults: { format: :json }, path: '',
+            path_names: { sign_in: 'login', sign_out: 'logout', registration: 'register' }
 
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
