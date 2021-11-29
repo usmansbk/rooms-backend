b1824cb70729a53ccdf5dc2d8eb1da0f364d5db6
Usman Suleiman <usmansbk@gmail.com>
2021-11-29 01:16:40 +0100

    Create auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, defaults: { format: :json }
+  devise_for :users, skip: :all
+  devise_scope :user do
+    post 'login', to: 'devise/sessions#create', defaults: { format: :json }
+    delete 'logout', to: 'devise/sessions#destroy', defaults: { format: :json }
+    post 'register', to: 'devise/registrations#create', defaults: { format: :json }
+  end
+
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
