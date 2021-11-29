8b04ed7809f7d127bcbba61baa8cd7b51a937537
Usman Suleiman <usmansbk@gmail.com>
2021-11-29 21:22:08 +0100

    Test model

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    subject { build(:room) }
+
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:price) }
+    it { should validate_presence_of(:size) }
+    it { should validate_presence_of(:bed_type) }
+    it { should validate_presence_of(:facilities) }
+    it { should validate_presence_of(:picture) }
+
+    it { should validate_length_of(:name) }
+    it { should validate_length_of(:city) }
+
+    it { should validate_numericality_of(:size) }
+    it { should validate_numericality_of(:price) }
+  end
+
+  describe 'associations' do
+    subject { build(:room) }
+
+    it { should have_many(:reservation) }
+    it { should belong_to(:user) }
+  end
 end
