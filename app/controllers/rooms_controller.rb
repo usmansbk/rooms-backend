a01a2691e3453f91ab1f980ad79f65692dc6650f
olawale-o <omoogunolawale@gmail.com>
2021-11-30 10:12:13 +0100

    Add generate RoomsController

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,38 @@
+class RoomsController < ApplicationController
+  before_action :set_room, only: :destroy
+
+  # GET /rooms
+  def index
+    @rooms = Room.all
+
+    render json: @rooms
+  end
+
+  # POST /rooms
+  def create
+    @room = Room.new(room_params)
+
+    if @room.save
+      render json: @room, status: :created, location: @room
+    else
+      render json: @room.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /rooms/1
+  def destroy
+    @room.destroy
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_room
+    @room = Room.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def room_params
+    params.fetch(:room, {})
+  end
+end
