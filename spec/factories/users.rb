1c91f827e15332fa5f0fcf6f0975cff65fecc62e
Usman Suleiman <usmansbk@gmail.com>
2021-11-30 14:07:54 +0100

    Create user association factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,29 @@ FactoryBot.define do
     name { Faker::Name.name }
     email { Faker::Internet.unique.email }
     password { Faker::Internet.password }
+
+    factory :user_with_rooms do
+      transient do
+        rooms_count { 5 }
+      end
+
+      after(:create) do |user, evaluator|
+        create_list(:room, evaluator.rooms_count, user: user)
+
+        user.reload
+      end
+    end
+
+    factory :user_with_reservations do
+      transient do
+        reservations_count { 5 }
+      end
+
+      after(:create) do |user, evaluator|
+        create_list(:reservation, evaluator.reservations_count, user: user)
+
+        user.reload
+      end
+    end
   end
 end
