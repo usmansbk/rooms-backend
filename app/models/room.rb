89243188429ee563cf6b37bc137a4094bdf812fc
olawale-o <omoogunolawale@gmail.com>
2021-11-30 10:01:51 +0100

    Add validations to room and reservation models

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,8 @@
 class Room < ApplicationRecord
+  validates :name, :city, length: { maximum: 100 }
+  validates :name, :city, :price, :size, :bed_type, :facilities, :picture, presence: true
+  validates :size, :price, numericality: { greater_than: 0 }
+
   belongs_to :user
   has_many :reservation
 end
