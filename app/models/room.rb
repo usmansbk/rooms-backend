30d19557abd060c1b294ec72d74d08c69b2667e9
Usman Suleiman <usmansbk@gmail.com>
2021-11-30 23:19:44 +0100

    Update validation

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,7 @@
 class Room < ApplicationRecord
   validates :name, :city, length: { maximum: 100 }
   validates :name, :city, :price, :size, :bed_type, :facilities, :picture, presence: true
-  validates :size, :price, numericality: { greater_than: 0 }
+  validates :size, :price, numericality: { only_integer: true, greater_than: 0 }
 
   belongs_to :user
   has_many :reservations
