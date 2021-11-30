a2f3a65142c935ae928d1b0e89aa03d5ab376739
Usman Suleiman <usmansbk@gmail.com>
2021-11-30 12:31:46 +0100

    Test reservation model

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    subject { build(:reservation) }
+
+    it { should validate_presence_of(:nights) }
+    it { should validate_presence_of(:check_in) }
+
+    it { should validate_numericality_of(:nights) }
+  end
+
+  describe 'associations' do
+    subject { build(:reservation) }
+
+    it { should belong_to(:room) }
+    it { should belong_to(:user) }
+  end
 end
