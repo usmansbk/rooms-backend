4236cc1cc47c3a1872dc9228c7f049085d28a00c
Usman Suleiman <usmansbk@gmail.com>
2021-11-30 14:57:18 +0100

    Fix format

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :reservations
-  resources :rooms
   devise_for :users, defaults: { format: :json }, path: '',
             path_names: { sign_in: 'login', sign_out: 'logout', registration: 'register' }
 
   mount Rswag::Ui::Engine => '/'
   mount Rswag::Api::Engine => '/api-docs'
+
+  resources :reservations, defaults: { format: :json }
+  resources :rooms, defaults: { format: :json }
 end
