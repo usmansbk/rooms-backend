a3d06b2504c9406536c38ae2c39e0545af297357
denisdiaconu <denisdiaconu117@yahoo.com>
2021-12-01 09:17:20 +0100

    Add rooms test user

--- spec/requests/rooms_spec.rb
+++ spec/requests/rooms_spec.rb
@@ -8,5 +8,24 @@ RSpec.describe 'rooms', type: :request do
         run_test!
       end
     end
+
+    post('create room') do
+      tags 'Room'
+      consumes 'multipart/form-data'
+      parameter name: :room, in: :formData, required: true, schema: {
+        type: :object,
+        properties: { name: { type: :string }, city: { type: :string },
+                      bed_type: { type: :string }, facilities: { type: :string },
+                      picture: { type: :file }, price: { type: :integer },
+                      size: { type: :integer } },
+        required: %w[name city bed_type facilities picture price size]
+      }
+
+      response(201, 'successful') do
+        login_user
+        let(:room) { attributes_for(:room) }
+        run_test!
+      end
+    end
   end
 end
