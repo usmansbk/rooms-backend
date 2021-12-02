249ded3b3ca4e5e1662bcf77a1b269953ad9e879
Usman Suleiman <usmansbk@gmail.com>
2021-12-02 22:34:05 +0100

    Expose authorization header

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'https://microverse-rooms.netlify.app', 'http://localhost:3000', 'http://127.0.0.1:3000'
-    resource '*', headers: :any, methods: :any 
+    resource '*', headers: %w(Authorization), expose: %w(Authorization), methods: :any, max_age: 600 
   end
 end
