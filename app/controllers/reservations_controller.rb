819064f4ca7b41b9004695f6583af6a56c3c93d6
olawale-o <omoogunolawale@gmail.com>
2021-12-02 10:29:26 +0100

    Modify ReservationControllers

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,14 +2,12 @@ class ReservationsController < ApplicationController
   load_and_authorize_resource
   before_action :set_reservation, only: :destroy
 
-  # GET /reservations
   def index
     @reservations = Reservation.all
 
     render json: @reservations
   end
 
-  # POST /reservations
   def create
     @reservation = current_user.reservations.new(reservation_params)
 
@@ -20,19 +18,16 @@ class ReservationsController < ApplicationController
     end
   end
 
-  # DELETE /reservations/1
   def destroy
     @reservation.destroy
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_reservation
     @reservation = Reservation.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def reservation_params
     params.fetch(:reservation).permit(:nights, :check_in, :room_id)
   end
