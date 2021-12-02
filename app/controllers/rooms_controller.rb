f0ed3532394a11acdae04c1a78e86a71a59f8d34
olawale-o <omoogunolawale@gmail.com>
2021-12-02 10:28:02 +0100

    Modify RoomsController

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,19 +3,16 @@ class RoomsController < ApplicationController
   before_action :set_room, only: %i[show destroy]
   skip_before_action :authenticate_user!, only: %i[index show]
 
-  # GET /rooms
   def index
     @rooms = Room.all
 
     render json: @rooms
   end
 
-  # GET /rooms/1
   def show
     render json: @room
   end
 
-  # POST /rooms
   def create
     @room = current_user.rooms.new(room_params)
 
@@ -26,19 +23,16 @@ class RoomsController < ApplicationController
     end
   end
 
-  # DELETE /rooms/1
   def destroy
     @room.destroy
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_room
     @room = Room.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def room_params
     params.fetch(:room).permit(:name, :city, :price, :size, :bed_type, :facilities, :picture)
   end
